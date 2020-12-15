e4f264ca52ce442e7166c20238e9712afff38dd1
Matthew Johnson <mattjo@microsoft.com>
2020-12-14 16:59:07 -0800

    Improvements to CanvasUtility to ensure PointerUp and PointerDown behavior is paired

--- Assets/MRTK/Services/InputSystem/Utilities/CanvasUtility.cs
+++ Assets/MRTK/Services/InputSystem/Utilities/CanvasUtility.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -14,24 +16,19 @@ namespace Microsoft.MixedReality.Toolkit.Input.Utilities
     [AddComponentMenu("Scripts/MRTK/Services/CanvasUtility")]
     public class CanvasUtility : MonoBehaviour, IMixedRealityPointerHandler
     {
-        private bool oldIsTargetPositionLockedOnFocusLock = false;
         public void OnPointerClicked(MixedRealityPointerEventData eventData) { }
 
         public void OnPointerDown(MixedRealityPointerEventData eventData)
         {
-            oldIsTargetPositionLockedOnFocusLock = eventData.Pointer.IsTargetPositionLockedOnFocusLock;
             if (!(eventData.Pointer is IMixedRealityNearPointer) && eventData.Pointer.Controller.IsRotationAvailable)
             {
-                eventData.Pointer.IsTargetPositionLockedOnFocusLock = false;
+                PointerUpHandler.TrackPointerUp(eventData.Pointer);
             }
         }
 
         public void OnPointerDragged(MixedRealityPointerEventData eventData) { }
 
-        public void OnPointerUp(MixedRealityPointerEventData eventData)
-        {
-            eventData.Pointer.IsTargetPositionLockedOnFocusLock = oldIsTargetPositionLockedOnFocusLock;
-        }
+        public void OnPointerUp(MixedRealityPointerEventData eventData) { }
 
         private void Start()
         {
@@ -59,6 +56,48 @@ namespace Microsoft.MixedReality.Toolkit.Input.Utilities
             else
             {
                 Debug.LogError("World Space Canvas should have no camera set to work properly with Mixed Reality Toolkit. At runtime, they'll get their camera set automatically.");
+            }
+        }
+
+        private class PointerUpHandler : IMixedRealityPointerHandler
+        {
+            private readonly IMixedRealityPointer pointer;
+            private readonly bool oldIsTargetPositionLockedOnFocusLock;
+
+            public static void TrackPointerUp(IMixedRealityPointer pointer)
+            {
+                var handler = new PointerUpHandler(pointer);
+                CoreServices.InputSystem.RegisterHandler<IMixedRealityPointerHandler>(handler);
+            }
+
+            private PointerUpHandler(IMixedRealityPointer pointer)
+            {
+                this.pointer = pointer;
+                this.oldIsTargetPositionLockedOnFocusLock = pointer.IsTargetPositionLockedOnFocusLock;
+
+                this.pointer.IsTargetPositionLockedOnFocusLock = false;
+            }
+
+            public void OnPointerClicked(MixedRealityPointerEventData eventData)
+            {
+            }
+
+            public void OnPointerDown(MixedRealityPointerEventData eventData)
+            {
+            }
+
+            public void OnPointerDragged(MixedRealityPointerEventData eventData)
+            {
+            }
+
+            public void OnPointerUp(MixedRealityPointerEventData eventData)
+            {
+                if (eventData.Pointer == this.pointer)
+                {
+                    this.pointer.IsTargetPositionLockedOnFocusLock = this.oldIsTargetPositionLockedOnFocusLock;
+
+                    CoreServices.InputSystem.UnregisterHandler<IMixedRealityPointerHandler>(this);
+                }
             }
         }
     }
